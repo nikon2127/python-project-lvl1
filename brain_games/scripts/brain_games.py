684172f3916fd8fcf564b04d6de55a689d340d13
@nikon2127 <nikon2127@gmail.com>
2022-04-20 09:32:25 +0300

    add def cli

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
-#!usr/bin/env python
+#!usr/bin/env python3
+import sys
+sys.path.insert(0, '/home/nikon/python-project-lvl1')
+from brain_games import cli
+
 def main():
-    print('Welcome to the Brain Games!')
+    cli.welcome_user()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
