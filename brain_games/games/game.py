a233fab6766698d7127add9bf8ee1c076a42275b
@nikon2127 <nikon2127@gmail.com>
2022-04-22 11:28:26 +0300

    fix game even

--- brain_games/games/game.py
+++ brain_games/games/game.py
@@ -2,44 +2,18 @@ import prompt
 import random
 
 
+def hello():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
+
 def even(min_num, max_num):
     num = random.randint(min_num, max_num)
     print(f'Question: {num}')
     input_user = prompt.string('Your answer: ')
     return (input_user, num)
-
-
-def eveno(name):
-    min_num = 1
-    max_num = 100
-    num = 0
-    max_points = 3
-    points = 0
-
-    while points < max_points:
-        num = random.randint(min_num, max_num)
-        print(f'Question: {num}')
-        input_user = prompt.string('Your answer: ')
-        if num % 2 == 0:
-            if input_user == 'yes':
-                print('Correct!')
-            else:
-                print(f"'{input_user}' is wrong answer ;(. \
-                    Correct answer was 'yes'.")
-                print(f"Let's try again, {name}!")
-                break
-        if num % 2 != 0:
-            if input_user == 'no':
-                print('Correct!')
-            else:
-                print(f"'{input_user}' is wrong answer ;(. \
-                    Correct answer was 'no'.")
-                print(f"Let's try again, {name}!")
-                break
-        points += 1
-        if points == max_points:
-            print(f'Congratulations, {name}!')
-            break
 
 
 def win_check(name, input_user, correct_answer):
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,20 +1,17 @@
 #!usr/bin/env python3
-from brain_games.games.hello_games import hello
 from brain_games.games import game
 
 
 def main():
-    name = hello()
+    name = game.hello()
     max_points = 3
     points = 0
 
     print('Answer "yes" if the number is even, otherwise answer "no".')
     while points < max_points:
         input_user, num = game.even(1, 100)
-        if game.win_check(name, input_user, num) == 'Correct':
-            print(game.win_check(name, input_user, num))
-        else:
-            print(game.win_check(name, input_user, num))
+        print(game.win_check(name, input_user, num))
+        if game.win_check(name, input_user, num) != 'Correct':
             break
         points += 1
         if points == max_points:
