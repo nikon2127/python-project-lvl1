106189a830dfef64bec52fa80f756e9fad0fa342
@nikon2127 <nikon2127@gmail.com>
2022-04-26 11:38:26 +0300

    add brain_calc.py

--- brain_games/games/game.py
+++ brain_games/games/game.py
@@ -24,6 +24,27 @@ Let's try again, {name}!", False)
 Let's try again, {name}!", False)
 
 
+def calc(name, min_num, max_num):
+    num1 = random.randint(min_num, max_num)
+    num2 = random.randint(min_num, max_num)
+    operand_rand = random.randint(1, 3)
+    if operand_rand == 1:
+        correct_answer = num1 + num2
+        operand = '+'
+    elif operand_rand == 2:
+        correct_answer = num1 * num2
+        operand = '*'
+    else:
+        correct_answer = num1 - num2
+        operand = '-'
+    print(f'Question: {num1}{operand}{num2}')
+    input_user = prompt.string('Your answer: ')
+    if input_user == str(correct_answer):
+        return ('Correct!', True)
+    return (f"'{input_user}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n\
+Let's try again, {name}!", False)
+
+
 def win_check(name, input_user, correct_answer):
     if input_user == correct_answer \
         or (int(correct_answer) % 2 == 0 and input_user == 'yes') \
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+from brain_games.games import game
+
+
+def main():
+    name = game.hello()
+    max_points = 3
+    points = 0
+
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while points < max_points:
+        report, is_check_win = game.calc(name, 1, 10)
+        print(report)
+        if not is_check_win:
+            break
+        points += 1
+        game.finish(name, points, max_points)
+
+
+if __name__ == '__main__':
+    main()
